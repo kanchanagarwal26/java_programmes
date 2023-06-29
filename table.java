a0c3693b6fccdf30d430c8bc6dbb374dcf4f15e1
Kanchan Agarwal <124901521+kanchanagarwal26@users.noreply.github.com>
2023-06-29 17:00:16 +0530

    by for loop

---
+++ table.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class table {
+    public static void main(String[]args){
+        Scanner sc =new Scanner(System.in);
+        int n =sc.nextInt();
+        int  i=0;
+        
+        for(i=1;i<=10;i++){
+            System.out.println(n*i);
+        }
+    
+        sc.close();
+        }
+    
+    }
